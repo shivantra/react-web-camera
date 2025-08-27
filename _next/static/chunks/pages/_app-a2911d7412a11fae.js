764b7ae17595f98de30260b6fa0c2c0d00860afa
Shivantra <dev@shivantra.com>
2025-08-27 18:48:49 +0530

    Updates

--- _next/static/chunks/pages/_app-a2911d7412a11fae.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4378:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(5175)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4378),n(7263))),_N_E=_.O()}]);
---
+++ _next/static/chunks/pages/_app-a3ca044f39560f99.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{6061:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(1859)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(6061),n(8875))),_N_E=_.O()}]);
